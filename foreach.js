33936e36af1dda51c4a8b8e631ff08114c0caafd
Your Namesifathero <sifathero466@gmail.com>
2024-03-22 19:09:34 +0600

    foreach

---
+++ foreach.js
@@ -0,0 +1,11 @@
+const number =[1,2,3,4,5,6,7,8];
+const result =number.forEach(n => console.log(n));
+console.log(result);
+
+// filter. its selected some condition and result put on a array  
+const filter =[9,8,7,6,5,4];
+const filt =filter.filter( n=> n>5);
+const filts =filter.filter( n=> n %2===0);
+console.log(filts)
+
+// find R filter holo same fine kono kico detacted korte na parle undefine dekhabe r filter hole seta empty array dekhabe 
