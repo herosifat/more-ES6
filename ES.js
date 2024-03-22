a29fb3c8643a5058fbc8423662c0b74e94738670
Your Namesifathero <sifathero466@gmail.com>
2024-03-22 16:17:43 +0600

    ES6

---
+++ ES.js
@@ -0,0 +1,14 @@
+// Data access
+const data =[{id:1, name:'imon',Address :'khilgon'}];
+console.log(data[0].Address);
+
+// more 
+const product ={
+    count :5000,
+    data :[
+        {id:1, name:'laptop', price :56000},
+        {id:1, name:'laptop', price :156000}
+    
+    ]
+}
+console.log(product.data[0].price);
